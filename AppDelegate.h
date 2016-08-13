d07bbe9064b1ede614661ae9ca6dfe0b6d2a5c90
LYB <leojr@163.com>
2016-08-13 17:49:03 +0800

    hehe
    nimbi

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quartz2D
+//
+//  Created by iLeo-OC on 16/8/11.
+//  Copyright © 2016年 iLeo-OC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
